eede9435be13568efd39287dd52b28a9f678c9b5
choi2601 <byeonghyeonchoe55@gmail.com>
2023-10-06 23:33:11 +0900

    feat(field/date): 달력 헤더 커스터마이징

--- components/common/field/date.tsx
+++ components/common/field/date.tsx
@@ -1,11 +1,49 @@
+import { getMonth, getYear, startOfDay, add } from "date-fns";
 import DatePicker from "react-datepicker";
+import type { ReactDatePickerCustomHeaderProps } from "react-datepicker";
 import styled, { css } from "styled-components";
 import { commonFieldStyle } from "./shared";
+import { SpacerSkleton } from "../spacer";
 
-export function DateField() {
+interface Props {
+  placeholder?: string;
+}
+
+const now = new Date();
+
+function CustomDatePickerHeader({
+  date,
+  decreaseMonth,
+  increaseMonth,
+  prevMonthButtonDisabled,
+  nextMonthButtonDisabled,
+}: ReactDatePickerCustomHeaderProps) {
+  return (
+    <HeaderContainer justify="center" align="center" gap={10}>
+      <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+        이전 날짜
+      </button>
+      <div id="date-info">{`${getYear(date)}.${getMonth(
+        add(date, { months: 1 })
+      )}`}</div>
+      <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+        다음 날짜
+      </button>
+    </HeaderContainer>
+  );
+}
+
+export function DateField({ placeholder = "날짜 선택" }: Props) {
   return (
     <Container>
-      <StyledDatePicker onChange={(date) => {}} />
+      <StyledDatePicker
+        renderCustomHeader={(props) => <CustomDatePickerHeader {...props} />}
+        placeholderText={placeholder}
+        minDate={startOfDay(now)}
+        onChange={(date) => {
+          console.log(date);
+        }}
+      />
     </Container>
   );
 }
@@ -22,6 +60,16 @@ const Container = styled.div`
       & > .react-datepicker__tab-loop {
         .react-datepicker-popper {
           .react-datepicker {
+            .react-datepicker__header {
+              margin-top: 5px;
+              background: none;
+              border: none;
+
+              .react-datepicker__day-names {
+                display: none;
+              }
+            }
+
             .react-datepicker__triangle {
               display: none;
             }
@@ -39,11 +87,33 @@ const StyledDatePicker = styled(DatePicker)`
     return css`
       ${commonFieldStyle};
 
+      text-align: center;
       cursor: pointer;
       caret-color: transparent;
 
       &:focus {
         outline: none;
+      }
+    `;
+  }}
+`;
+
+const HeaderContainer = styled(SpacerSkleton)`
+  ${({ theme }) => {
+    const { colors } = theme;
+
+    return css`
+      & > button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        border: none;
+        background: none;
+      }
+
+      & > div[id="date-info"] {
+        color: ${colors.primary_02};
       }
     `;
   }}
