2f0315c626253af1cea831770951211fdaa12442
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2023-12-09 15:31:28 +0900

    feat(theme/colors): 변경된 디자인 시스템 색상 적용

--- styles/theme/colors.ts
+++ styles/theme/colors.ts
@@ -1,4 +1,5 @@
 const colors = {
+  // legacy color system
   primary_01: "#4F9EF7",
   primary_02: "#486284",
   error: "#FF0000",
@@ -23,6 +24,23 @@ const colors = {
   black_03: "#1e1e1e",
   black_04: "#111111",
   black_05: "#000000",
+  // updated color system
+  primary: {
+    point: "#FF9900",
+  },
+  secondary: {
+    black: "#0A0A0A",
+    white: "#FFFFFF",
+    900: "#1D1D1D",
+    500: "#505050",
+    400: "#A6A6A6",
+    200: "#D4D4D4",
+    150: "#E8E8E8",
+    100: "#F9F9F9",
+  },
+  system: {
+    red: "#DD2424",
+  },
 } as const;
 
 export default colors;
