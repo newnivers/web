c4c8e60fcb2c63a1bf43f3a19fae004c45ecda3a
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2023-12-31 18:50:52 +0900

    feat(detailInfo/imageRegister): 등록된 이미지 제거 기능 추가

--- components/domains/register-work/form/content/detailInfo/imageRegister/index.tsx
+++ components/domains/register-work/form/content/detailInfo/imageRegister/index.tsx
@@ -1,8 +1,7 @@
-import type { ChangeEvent } from "react";
 import Image from "next/image";
 import type { UseControllerProps } from "react-hook-form";
 import { useController } from "react-hook-form";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { SpacerSkleton } from "@/components/common/spacer";
 import Typography from "@/components/common/text/Typography";
 import { useFileUpload } from "@/hooks";
@@ -10,16 +9,41 @@ import { useFileUpload } from "@/hooks";
 export function ImageRegister({ ...controllerProps }: UseControllerProps) {
   const { field } = useController(controllerProps);
 
-  const { fileInfo, onChangeFile } = useFileUpload({
+  const { fileInfo, onChangeFile, resetFile } = useFileUpload({
     onFileChangedSuccess(fileInfo) {
       field.onChange(fileInfo);
     },
   });
 
+  const onClickImageDelete = () => {
+    resetFile();
+    field.onChange({ name: "", data: null });
+  };
+
   return (
     <>
       {fileInfo.data ? (
-        <RegisteredImageInfo>이미지등록</RegisteredImageInfo>
+        <RegisteredImageInfo>
+          <RowHeader justify="space-between" align="center">
+            <FileTypography typo="subhead05">파일명</FileTypography>
+            {/* <SpacerSkleton gap={26}></SpacerSkleton> */}
+          </RowHeader>
+          <ImageInfoContent justify="space-between" align="center">
+            <SpacerSkleton align="center" gap={8}>
+              <button onClick={onClickImageDelete}>
+                <Image
+                  src="/icon/default-close.svg"
+                  width={16}
+                  height={16}
+                  alt="default-close"
+                />
+              </button>
+              <ImageNameTypography typo="subhead05">
+                {fileInfo.name}
+              </ImageNameTypography>
+            </SpacerSkleton>
+          </ImageInfoContent>
+        </RegisteredImageInfo>
       ) : (
         <>
           <ImageRegisterLabel
@@ -51,11 +75,15 @@ export function ImageRegister({ ...controllerProps }: UseControllerProps) {
   );
 }
 
-const ImageRegisterLabel = styled(SpacerSkleton)`
+const commonContainerStyle = () => css`
   flex: 1;
-  padding: 24px;
   border: 1px solid ${({ theme: { colors } }) => colors.secondary[200]};
   border-radius: 12px;
+`;
+
+const ImageRegisterLabel = styled(SpacerSkleton)`
+  ${commonContainerStyle()}
+  padding: 24px;
   cursor: pointer;
 `;
 
@@ -72,4 +100,29 @@ const ImageGuideTypography = styled(Typography)`
   color: ${({ theme: { colors } }) => colors.secondary[400]};
 `;
 
-const RegisteredImageInfo = styled.div``;
+const RegisteredImageInfo = styled.div`
+  ${commonContainerStyle()}
+  height: 73px;
+`;
+
+const RowHeader = styled(SpacerSkleton)`
+  padding: 7px 12px;
+  border-top-left-radius: 12px;
+  border-top-right-radius: 12px;
+  background-color: ${({ theme: { colors } }) => colors.secondary[200]};
+`;
+
+const FileTypography = styled(Typography)`
+  color: ${({ theme: { colors } }) => colors.secondary[400]};
+`;
+
+const ImageInfoContent = styled(SpacerSkleton)`
+  padding: 8px 12px;
+  border-bottom-left-radius: 12px;
+  border-bottom-right-radius: 12px;
+  background-color: ${({ theme: { colors } }) => colors.secondary.white};
+`;
+
+const ImageNameTypography = styled(Typography)`
+  color: ${({ theme: { colors } }) => colors.secondary[500]};
+`;
