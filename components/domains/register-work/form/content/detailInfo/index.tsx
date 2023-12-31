6c667b278ebd936633b8ababd9200abbd99d1c8b
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2023-12-31 19:54:07 +0900

    style(common/textEditor): 텍스트 에디터 디자인 사항에 맞게 수정
    ISSUES CLOSED: #88

--- components/domains/register-work/form/content/detailInfo/index.tsx
+++ components/domains/register-work/form/content/detailInfo/index.tsx
@@ -1,7 +1,10 @@
 import type { FieldValues } from "react-hook-form";
 import { useForm } from "react-hook-form";
+import { css } from "styled-components";
 import { TitleColumn, InputColumn } from "@/components/common/column";
 import { SpacerSkleton } from "@/components/common/spacer";
+import TextEditor from "@/components/common/textEditor";
+import { defaultDescription, defaultCautionDescription } from "./data";
 import { ImageRegister } from "./imageRegister";
 import type { FormContentProps } from "../type";
 
@@ -9,6 +12,8 @@ export function DetailInfo({ classifications }: FormContentProps) {
   const { control } = useForm<FieldValues>({
     defaultValues: {
       image: { name: "", data: null },
+      description: defaultDescription,
+      caution_description: defaultCautionDescription,
     },
   });
 
@@ -21,6 +26,46 @@ export function DetailInfo({ classifications }: FormContentProps) {
         spacer={{ align: "start", gap: 30 }}
       >
         <ImageRegister control={control} name="image" />
+      </InputColumn>
+      <InputColumn
+        id="description"
+        name="상세 페이지"
+        spacer={{ align: "start", gap: 30 }}
+      >
+        <TextEditor
+          defaultValue={defaultDescription}
+          containerStyle={{ width: "100%" }}
+          qlEditorStyle={css`
+            .ql-editor {
+              min-height: fit-content;
+            }
+            .ql-toolbar.ql-snow {
+              display: flex;
+            }
+          `}
+          _onContentChange={(res) => {}}
+          _onImageUpload={(res) => {}}
+        />
+      </InputColumn>
+      <InputColumn
+        id="caution_description"
+        name="예매 유의사항"
+        spacer={{ align: "start", gap: 16 }}
+      >
+        <TextEditor
+          defaultValue={defaultCautionDescription}
+          containerStyle={{ width: "100%" }}
+          qlEditorStyle={css`
+            .ql-editor {
+              min-height: fit-content;
+            }
+            .ql-toolbar.ql-snow {
+              display: flex;
+            }
+          `}
+          _onContentChange={(res) => {}}
+          _onImageUpload={(res) => {}}
+        />
       </InputColumn>
     </SpacerSkleton>
   ));
