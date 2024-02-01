edd0219b1ad3907c050f27ac1af03a9e9ae14f07
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2024-02-01 21:28:09 +0900

    style(domain/enrollment-check): 등록확인 삭제 col 제거
    ISSUES CLOSED: #131

--- components/domains/enrollment-check/table/index.tsx
+++ components/domains/enrollment-check/table/index.tsx
@@ -17,13 +17,13 @@ const column: ColumnDef<any>[] = [
     accessorFn: (row) => row.created_at,
     size: 84,
   },
-  { id: "genre", header: "장르", accessorFn: (row) => row.genre, size: 70 },
+  { id: "genre", header: "장르", accessorFn: (row) => row.genre, size: 80 },
   { id: "title", header: "작품명", accessorFn: (row) => row.title, size: 245 },
   {
     id: "createdBy",
     header: "관리자명",
     accessorFn: (row) => row.createdBy,
-    size: 58,
+    size: 60,
   },
   {
     id: "status",
@@ -37,15 +37,15 @@ const column: ColumnDef<any>[] = [
     accessorFn: (row) => row.selling,
     size: 132,
   },
-  {
-    id: "admin",
-    header: "관리",
-    accessorFn: (row) => row.admin,
-    size: 72,
-    cell(props) {
-      return <DeleteAdmin />;
-    },
-  },
+  // {
+  //   id: "admin",
+  //   header: "관리",
+  //   accessorFn: (row) => row.admin,
+  //   size: 72,
+  //   cell(props) {
+  //     return <DeleteAdmin />;
+  //   },
+  // },
 ];
 
 export function EnrollmentCheckTable() {
@@ -98,6 +98,6 @@ export function EnrollmentCheckTable() {
 }
 
 const TableContainer = styled.div`
-  height: 600px;
+  max-height: 600px;
   overflow: scroll;
 `;
